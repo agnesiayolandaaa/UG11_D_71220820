0ad6de60158a818be9406c3f75208b74aa323aa2
agnesiayolanda <117900996+agnesiayolandaaa@users.noreply.github.com>
2022-12-01 16:16:11 +0700

    Add files via upload

---
+++ 1_UG11_71220820.py
@@ -0,0 +1,33 @@
+print ("Halo selamat datang dibioskop:")
+print ("Dimanakah anda ingin menonton?:")
+
+if tempat < bioskop:
+    print ("1). XXI EMPIRE ")
+else:
+    print ("2).XXI AMPLAS ")
+else:
+    print  ("3). XXI JCM :")1
+
+bioskop =int(input ("Masukkan pilihanmu :")) 
+
+print ("Mau nonton film apa nih? Ada film:")
+print ("1). Frozen 2).Keramat 3). KKN di Desa Penari:")
+
+film = int(input ("Pilih no film:"))
+
+print ("Mau nonton layar bioskop apa:")
+if (layarbioskop == "layar"):
+    print ("1). Reguler ")
+elif (layarbioskop == "layar"):
+    print ("2). Dolby Amos ")
+elif (layarbioskop == "layar"):
+    print (" 3). Premiere:")
+
+layar = int(input("Pilih no tipe layar:"))
+
+print = input("Berapa banyak tiket?:")
+
+print ("Mau nonton layar bioskop apa:")
+print ("1). 12.35 2). 14.45 3). 16.55 4) 19.05:")
+
+layar = int(input("Masukkan angka pilihan anda:"))
